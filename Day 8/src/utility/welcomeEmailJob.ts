a4a269290c83766ef7a9442b1573e534ecf6c41a
patoliya-ap1 <patoliya.ap1@gmail.com>
2026-03-13 15:02:18 +0530

    ref -change local-redis to remote-redis

--- Day 8/src/utility/welcomeEmailJob.ts
+++ Day 8/src/utility/welcomeEmailJob.ts
@@ -1,7 +1,10 @@
 import { emailQueue } from "../queue/emailQueue.js";
 
-export const welcomeEmailJob = async (jobData:{email:string,subject:string,template:string}) => {
-  console.log(jobData);
+export const welcomeEmailJob = async (jobData: {
+  email: string;
+  subject: string;
+  template: string;
+}) => {
   try {
     await emailQueue.add(
       "sendWelcomeEmail",
@@ -16,4 +19,4 @@ export const welcomeEmailJob = async (jobData:{email:string,subject:string,templ
   } catch (error) {
     console.log("Failed to add Welcome email job to queue");
   }
-}
+};
